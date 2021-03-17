871b77076d560e8e5cd95fb793cca8a530f193f5
Nacho <nachosoloaga@gmail.com>
2021-03-16 23:16:35 -0300

    Added background image

--- src/modules/Dashboard.jsx
+++ src/modules/Dashboard.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import Weather from '../components/weather/Weather';
-import Quotes from '../components/quotes/Quotes';
 import Crypto from '../components/crypto/Crypto';
-import Datetime from '../components/datetime/Datetime';
+
 import './dashboard.css';
+import Background from '../images/background.jpg';
+
+const search = (event, searchTerm) => {
+  event.preventDefault();
+  window.location.href = `https://www.google.com/search?q=${encodeURIComponent(
+    searchTerm
+  )}`;
+};
 
 const Dashboard = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `url(${Background})`,
+      }}
+    >
       <section className="top-section">
         <Weather />
-        <Quotes />
+        <Crypto />
       </section>
       <section className="bottom-section">
-        <Crypto />
-        <Datetime />
+        <form>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={(event) => setSearchTerm(event.target.value)}
+            ref={inputRef}
+          />
+          <button
+            type="submit"
+            onClick={(event) => search(event, searchTerm)}
+          ></button>
+        </form>
       </section>
     </div>
   );
